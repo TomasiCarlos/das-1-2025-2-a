15f12f51ee2efdb878442c5fd1ff5a1679d9879a
TomasiCarlos <carlostomasi52@gmail.com>
2025-08-25 21:46:54 -0300

    25/08/2025

--- designpatterns/src/main/java/br/univille/observer/Cliente.java
+++ designpatterns/src/main/java/br/univille/observer/Cliente.java
@@ -2,4 +2,18 @@ package br.univille.observer;
 
 public class Cliente {
 
+    public static void main(String[] args) {
+        var grupoFamilia = new Publisher();
+        
+        var zezinho = new ConcreteSubscribers();
+        var huguinho = new ConcreteSubscribers();
+        var tia = new ConcreteSubscribers();
+
+        grupoFamilia.subscribers(tia);
+        grupoFamilia.subscribers(huguinho);
+        grupoFamilia.subscribers(zezinho);
+
+        grupoFamilia.setMainState("Bom dia");
+        grupoFamilia.notifySubscriber();
+    }
 }
--- designpatterns/src/main/java/br/univille/observer/ConcreteSubscribers.java
+++ designpatterns/src/main/java/br/univille/observer/ConcreteSubscribers.java
@@ -1,10 +1,10 @@
 package br.univille.observer;
 
-public class ConcreteSubscribers implements Subscriber{
+public class ConcreteSubscribers 
+    implements Subscriber{
 
     @Override
-    public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public void update(String context) {
+            System.out.println(context);
+        }
     }
-}
